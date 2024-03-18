73b5beb41d3e9ceb9ca8787a00e5c52e8e3d5eb7
Hazrat Sharif <50118391+hazratsahrif@users.noreply.github.com>
2024-03-18 16:17:19 +0500

    started home screen

---
+++ app/src/main/java/com/example/freebird/presentation/ui/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.freebird.presentation.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.freebird.R;
+import com.example.freebird.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    FragmentHomeBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
+    }
+}
