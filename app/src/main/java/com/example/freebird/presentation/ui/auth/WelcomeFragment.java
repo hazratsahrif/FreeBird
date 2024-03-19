1c4124b555fe13964099c18da010a4787ed10ae5
Faizan Malik <malickfaizann@gmail.com>
2024-03-19 18:16:29 +0500

    Feat: SignIn screen in progress

--- app/src/main/java/com/example/freebird/presentation/ui/auth/WelcomeFragment.java
+++ app/src/main/java/com/example/freebird/presentation/ui/auth/WelcomeFragment.java
@@ -1,14 +1,7 @@
 package com.example.freebird.presentation.ui.auth;
 
-import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
-import com.example.freebird.R;
 import com.example.freebird.databinding.FragmentWelcomeBinding;
 import com.example.freebird.presentation.ui.base.BaseFragment;
 
@@ -21,8 +14,7 @@ public class WelcomeFragment extends BaseFragment<FragmentWelcomeBinding> {
 
     @Override
     public void setupView() {
-
-
+        binding.btnSignIn.setOnClickListener(view -> findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToSignInFragment()));
     }
 
     @Override
